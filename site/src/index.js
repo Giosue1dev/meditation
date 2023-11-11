e0e868ff989baf47e31d7cbfdd138d3247ae6f73
Giosue1dev <131166708+Giosue1dev@users.noreply.github.com>
2023-11-11 17:00:18 +0100

    cambiamenti

--- site/src/index.js
+++ site/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './component/css/index.css';
-import App from './App';
+import App from './component/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
